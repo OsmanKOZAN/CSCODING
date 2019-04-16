17740668db20fb94a4c360ad8b6eccda0c662065
OsmanKOZAN <cscoding@gmail.com>
2019-04-16 18:20:03 +0300

    Update Connect.py

--- SQLite/Connect.py
+++ SQLite/Connect.py
@@ -1,1 +1,18 @@
 
+# -*- coding: utf-8 -*-
+#%% Sqlite
+#kütüphaneyi import edelim
+import sqlite3
+
+con = sqlite3.connect("Kutuphane.db")
+
+cur = con.cursor()
+
+def tabloOlustur():
+    cur.execute("Create Table if not exists Kitaplar(ID int, AD text, YAZAR text, SAYFA int )")
+    con.commit()
+
+#Create Table
+tabloOlustur()
+
+con.close()
